d58e9183a862c74234ce4456fb72c62a4c38e96b
Zarah Floro <zbnfloro@gmail.com>
2024-12-13 11:48:22 +0800

    feat: landing page

---
+++ src/lib/components/navigation/Hero.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import up_logo from '$lib/assets/up-logo.png'
+    import dcs_logo from '$lib/assets/dcs-logo.png'
+    import engglib_logo from '$lib/assets/engglib-logo.png'
+</script>
+
+<section class="py-4 pl-4 w-full h-min bg-gray-200">
+    <div class="flex gap-4">
+        <img class="size-20" src={up_logo} alt="University of the Philippines Diliman logo."/>
+        <img class="size-20" src={dcs_logo} alt="University of the Philippines Diliman Department of Computer Science logo."/>
+        <img class="h-20" src={engglib_logo} alt="University of the Philippines Diliman College of Engineering Libraries logo."/>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Footer from '$lib/components/navigation/Footer.svelte';
+	import Hero from '$lib/components/navigation/Hero.svelte';
+</script>
+
+<div class="h-screen w-screen flex flex-col">
+	<!-- Hero -->
+	<Hero />
+
+	<!-- Login -->
+	<section class="h-full grow">
+        <p>Login</p>
+    </section>
+
+	<!-- Footer -->
+	<Footer />
+</div>
