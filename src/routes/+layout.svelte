0a27e165e7da8c349bd2bcab8f26f169600654e2
Allaine Tan <56602966+allainerain@users.noreply.github.com>
2025-01-01 14:19:03 +0800

    style: apply Inter as a global font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+	import PhotoCard from '$lib/components/PhotoCard.svelte';
+	// let { children } = $props();
 </script>
 
-{@render children()}
+<!-- {@render children()} -->
+<section class="w-max-screen grid h-screen grid-cols-2">
+	<!-- Photocard -->
+	<div class="h-full p-4">
+		<PhotoCard></PhotoCard>
+	</div>
+	<!-- Login/Register -->
+	<div><slot></slot></div>
+</section>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+</style>
