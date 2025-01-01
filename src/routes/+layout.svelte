9605560ffaa05b27e313048d47e97b54665b1ab1
Allaine Tan <56602966+allainerain@users.noreply.github.com>
2025-01-01 17:02:31 +0800

    feat: implement initial login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	import PhotoCard from '$lib/components/PhotoCard.svelte';
-	// let { children } = $props();
 </script>
 
-<!-- {@render children()} -->
-<section class="w-max-screen grid h-screen grid-cols-2">
-	<!-- Photocard -->
-	<div class="h-full p-4">
-		<PhotoCard></PhotoCard>
-	</div>
-	<!-- Login/Register -->
-	<div><slot></slot></div>
-</section>
+<slot></slot>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	// UI iIports
 	import { Input } from '$lib/components/ui/input';
+	import PhotoCard from '$lib/components/PhotoCard.svelte';
 	import * as Form from '$lib/components/ui/form';
 
 	// Backend Imports
@@ -75,49 +76,82 @@
 	onMount(startSession);
 </script>
 
-<div class="flex h-[80%] w-full items-center">
-	<!-- Select library and section to proceed to enable the website -->
-	<section class="flex w-full flex-col items-center px-[150px]">
-		{#if $UserStore.fullName}
-			<!-- Direct to dashboard -->
-			<h1>You are now logged in, {$UserStore.fullName}</h1>
-			<!-- Logout -->
-			<button on:click={endSession}>Log out</button>
-		{:else}
-			<!-- Login Text -->
-			<div class="flex w-full flex-col gap-8">
-				<div class="flex w-full flex-col gap-4">
-					<h1 class="text-5xl font-medium">Tap your UP ID to begin</h1>
-					<!-- <h2 class="text-lg font-normal">
-						Avail Engglib services using SUSê by tapping your RFID!
-					</h2> -->
-				</div>
-				<Input
-					type="password"
-					placeholder="Avail Engglib services using SUSê by tapping your RFID!"
-					class="h-[55px] max-w-full text-base"
-				/>
+<section class="grid h-screen max-h-dvh grid-cols-2">
+	<!-- Photocard -->
+	<div class="h-full p-4">
+		<PhotoCard></PhotoCard>
+	</div>
+
+	<!-- Login -->
+	<div class="h-full p-4">
+		<!-- Logo header -->
+		<div class="items-right flex h-[65px] w-full justify-end gap-4">
+			<img src="logos/up-logo.png" class="h-full w-auto" alt="University of the Philippines logo" />
+			<img
+				src="logos/dcs-logo.png"
+				class="h-full w-auto"
+				alt="Department of Computer Science logo"
+			/>
+			<img src="logos/engglib-logo.png" class="h-full w-auto" alt="Engineering Library logo" />
+		</div>
+
+		<!-- Login -->
+		<div class="flex h-[80%] w-full items-center">
+			<!-- Select library and section to proceed to enable the website -->
+			<div class="flex w-full flex-col items-center px-[150px]">
+				{#if $UserStore.fullName}
+					<!-- Direct to dashboard -->
+					<h1>You are now logged in, {$UserStore.fullName}</h1>
+					<!-- Logout -->
+					<button on:click={endSession}>Log out</button>
+				{:else}
+					<div class="flex w-full flex-col gap-4">
+						<!-- Login Text -->
+						<div class="flex w-full flex-col gap-8">
+							<div class="flex w-full flex-col gap-4">
+								<h1 class="text-5xl font-medium">Tap your UP ID to begin</h1>
+								<h2 class="text-lg font-normal">
+									Avail Engglib services using SUSê by tapping your RFID!
+								</h2>
+							</div>
+							<Input
+								type="text"
+								placeholder="••••••••••"
+								class="h-[55px] max-w-full text-center text-base"
+							/>
+						</div>
+
+						<!-- Divider -->
+						<div class="my-4 flex w-full items-center justify-center">
+							<div class="h-[0.5px] flex-grow bg-black/20"></div>
+							<!-- Left line -->
+							<span class="mx-4"><p>or</p></span>
+							<div class="h-[0.5px] flex-grow bg-black/20"></div>
+							<!-- Right line -->
+						</div>
+
+						<!-- Login -->
+						<form method="POST" class="w-full">
+							<Form.Button variant="outline" class="flex h-[55px] w-full gap-2">
+								<img src="logos/google.png" class="h-[70%]" alt="Google logo" />
+								<p class="text-base">Login with UP Mail</p>
+							</Form.Button>
+						</form>
+					</div>
+				{/if}
 			</div>
+		</div>
 
-			<!-- Divider -->
-			<div class="my-4 flex w-full items-center justify-center">
-				<div class="h-[0.5px] flex-grow bg-black/20"></div>
-				<!-- Left line -->
-				<span class="mx-4"><p>or</p></span>
-				<div class="h-[0.5px] flex-grow bg-black/20"></div>
-				<!-- Right line -->
-			</div>
-
-			<!-- Login -->
-			<form method="POST" class="w-full">
-				<Form.Button variant="outline" class="flex h-[55px] w-full gap-2">
-					<img src="logos/google.png" class="h-[70%]" alt="Google logo" />
-					<p class="text-base">Login with UP Mail</p>
-				</Form.Button>
-			</form>
-		{/if}
-	</section>
-
-	<!-- Footer -->
-	<!-- <Footer /> -->
-</div>
+		<!-- Footer -->
+		<div class="flex flex-col gap-4 px-[150px] text-center text-black/80">
+			<p class="text-base">
+				SUSe is developed by Zarah Floro and Allaine Tan for the College of Engineering Libraries.
+			</p>
+			<p class="text-xs">
+				Initially formulated along with Gabriel Calubayan and Antonio Torres for CS191/192 under
+				Prof. Rowena Solamo and continued with Victor Reyes, Michael Monasterio, and Julia Sabado
+				for Engg150 under Prof. Jude Agapito.
+			</p>
+		</div>
+	</div>
+</section>
