b3db0ef3540ac905664101e26f7c7e8be8de14fb
Allaine Tan <56602966+allainerain@users.noreply.github.com>
2025-01-02 00:09:07 +0800

    feat: make PhotoCard.svelte dynamic to library

--- src/lib/components/PhotoCard.svelte
+++ src/lib/components/PhotoCard.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import MapPin from 'lucide-svelte/icons/map-pin';
 
-	let imgSrc = 'photos/AECH.jpeg';
-	let libName = 'Engineering Library II';
-	let libBuilding = 'G/F UP Alumni Engineers Centennial Hall';
-	let libStreet = 'Velasquez St., UP Diliman';
+	export let libSrc;
+	export let libName;
+	export let libBuilding;
+	export let libStreet;
 </script>
 
 <div class="relative mx-auto h-full w-full overflow-hidden rounded-[10px]">
 	<!-- Gradient overlay -->
 	<div class="absolute inset-0 bg-gradient-to-t from-[#FF8000]/25 to-[#34C370]/10"></div>
 	<!-- Image -->
-	<img src={imgSrc} alt="The engineering library building" class="h-full object-cover" />
+	<img src={libSrc} alt="The engineering library building" class="h-full object-cover" />
 	<!-- Blur -->
 	<div
 		class="overlay-blur bg-[#D9D9D9]/.20 absolute inset-x-0 bottom-0 h-1/3 w-full rounded-t-[2000px] backdrop-blur-lg"
@@ -19,10 +19,10 @@
 	<!-- Blur -->
 	<div class="gap-[20px absolute inset-x-0 bottom-0 flex w-full flex-col gap-8 p-10 text-center">
 		<div class="flex w-full items-center justify-center gap-4">
-			<MapPin size="36" strokeWidth="2" color="#ffffff" />
-			<p class="text-4xl font-medium text-white">{libName}</p>
+			<MapPin size="32" strokeWidth="2" color="#ffffff" />
+			<p class="text-3xl font-medium text-white">{libName}</p>
 		</div>
-		<p class="text-lg font-normal text-white/[.80]">
+		<p class="text-base font-normal text-white/[.80]">
 			{libBuilding}<br />
 			{libStreet}
 		</p>
