594a01ef4dd9307929c462f34d047f6ce4286e8e
Allaine Tan <56602966+allainerain@users.noreply.github.com>
2025-01-01 14:18:37 +0800

    feat: add initial PhotoCard.svelte component

---
+++ src/lib/components/PhotoCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let imgSrc = 'photos/AECH.jpeg';
+	let libName = 'Engineering Library II';
+	let libBuilding = 'G/F UP Alumni Engineers Centennial Hall';
+	let libStreet = 'Velasquez St., UP Diliman';
+</script>
+
+<div class="relative mx-auto h-full w-full overflow-hidden rounded-[10px]">
+	<!-- Gradient overlay -->
+	<div class="absolute inset-0 bg-gradient-to-t from-[#FF8000]/25 to-[#34C370]/10"></div>
+	<!-- Image -->
+	<img src={imgSrc} alt="The engineering library building" class="h-full object-cover" />
+	<!-- Blur -->
+	<div
+		class="overlay-blur bg-[#D9D9D9]/.20 absolute inset-x-0 bottom-0 h-1/3 w-full rounded-t-[2000px] backdrop-blur-lg"
+	></div>
+	<!-- Blur -->
+	<div class="gap-[20px absolute inset-x-0 bottom-0 flex w-full flex-col gap-4 p-10 text-center">
+		<p class="text-3xl font-medium text-white">{libName}</p>
+		<p class="text-base font-normal text-white/[.80]">
+			{libBuilding}<br />
+			{libStreet}
+		</p>
+	</div>
+</div>
+
+<style>
+	.overlay-blur {
+		mask-image: radial-gradient(
+			ellipse 90% 100% at center 75%,
+			black 40%,
+			rgba(0, 0, 0, 0.2) 60%,
+			rgba(0, 0, 0, 0) 100%
+		);
+		mask-size: 100% 100%;
+	}
+</style>
