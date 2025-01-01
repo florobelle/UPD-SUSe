07ea78435b2acd091613c8ebbbf51e45c8f66957
Allaine Tan <56602966+allainerain@users.noreply.github.com>
2025-01-01 15:05:45 +0800

    style: fix style of initial login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	// UI iIports
+	import { Input } from '$lib/components/ui/input';
+	import * as Form from '$lib/components/ui/form';
+
+	// Backend Imports
 	import { page } from '$app/stores';
 	import { createCookie, deleteCookie, readCookie } from '$lib/client/Cookie';
 	import { UserStore } from '$lib/client/Stores/User';
 	import { supabaseClient } from '$lib/client/SupabaseClient';
-	import Footer from '$lib/components/navigation/Footer.svelte';
-	import Hero from '$lib/components/navigation/Hero.svelte';
 	import { onMount } from 'svelte';
 
 	async function startSession() {
@@ -40,7 +43,7 @@
 				return;
 			}
 		}
-        
+
 		$UserStore = {
 			authenticated: true,
 			fullName: user?.user_metadata.full_name ? user?.user_metadata.full_name : '',
@@ -54,7 +57,7 @@
 		// Ends the user's current session if available.
 		const { error } = await supabaseClient.auth.signOut();
 		deleteCookie('accessToken', $page.url.pathname),
-        deleteCookie('refreshToken', $page.url.pathname);
+			deleteCookie('refreshToken', $page.url.pathname);
 
 		$UserStore = {
 			authenticated: false,
@@ -72,25 +75,49 @@
 	onMount(startSession);
 </script>
 
-<div class="flex h-screen w-screen flex-col">
-	<!-- Hero -->
-	<Hero />
-
+<div class="flex h-[80%] w-full items-center">
 	<!-- Select library and section to proceed to enable the website -->
-	<section class="flex grow flex-col items-center">
+	<section class="flex w-full flex-col items-center px-[150px]">
 		{#if $UserStore.fullName}
+			<!-- Direct to dashboard -->
 			<h1>You are now logged in, {$UserStore.fullName}</h1>
+			<!-- Logout -->
+			<button on:click={endSession}>Log out</button>
 		{:else}
-			<h1>You are not logged in.</h1>
+			<!-- Login Text -->
+			<div class="flex w-full flex-col gap-8">
+				<div class="flex w-full flex-col gap-4">
+					<h1 class="text-5xl font-medium">Tap your UP ID to begin</h1>
+					<!-- <h2 class="text-lg font-normal">
+						Avail Engglib services using SUSê by tapping your RFID!
+					</h2> -->
+				</div>
+				<Input
+					type="password"
+					placeholder="Avail Engglib services using SUSê by tapping your RFID!"
+					class="h-[55px] max-w-full text-base"
+				/>
+			</div>
+
+			<!-- Divider -->
+			<div class="my-4 flex w-full items-center justify-center">
+				<div class="h-[0.5px] flex-grow bg-black/20"></div>
+				<!-- Left line -->
+				<span class="mx-4"><p>or</p></span>
+				<div class="h-[0.5px] flex-grow bg-black/20"></div>
+				<!-- Right line -->
+			</div>
+
+			<!-- Login -->
+			<form method="POST" class="w-full">
+				<Form.Button variant="outline" class="flex h-[55px] w-full gap-2">
+					<img src="logos/google.png" class="h-[70%]" alt="Google logo" />
+					<p class="text-base">Login with UP Mail</p>
+				</Form.Button>
+			</form>
 		{/if}
-		<!-- Login -->
-		<form method="POST">
-			<button>Login</button>
-		</form>
-		<!-- Logout -->
-		<button on:click={endSession}>Log out</button>
 	</section>
 
 	<!-- Footer -->
-	<Footer />
+	<!-- <Footer /> -->
 </div>
