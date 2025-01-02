d8125652315eb7638aa4883cfc2aad2ac76c8003
florobelle <zbnfloro@gmail.com>
2025-01-02 17:00:14 +0800

    feat: replace oauth with otp
    Replaced Google OAuth for email OTP instead for user privacy.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,13 @@
 	import { onMount } from 'svelte';
 	import toast, { Toaster } from 'svelte-5-french-toast';
 
-    let rfid: string = '';
+	// -----
+
+	let rfid: string = '';
+	let username: string = '';
+	let otp: string = '';
+
+	// -----
 
 	async function startSession() {
 		// Saves the user's access and refresh tokens in cookies and creates a new session if needed.
@@ -44,20 +50,20 @@
 				return;
 			}
 		}
-        
+
 		$UserStore = {
 			authenticated: true,
 			fullName: user?.user_metadata.full_name ? user?.user_metadata.full_name : '',
 			email: user?.email ? user?.email : ''
 		};
-        toast.success(`You're now logged in, ${$UserStore.fullName}!`)
+		toast.success(`You're now logged in, ${$UserStore.fullName}!`);
 	}
 
 	async function endSession() {
 		// Ends the user's current session if available.
 		const { error } = await supabaseClient.auth.signOut();
 		deleteCookie('accessToken', $page.url.pathname),
-        deleteCookie('refreshToken', $page.url.pathname);
+			deleteCookie('refreshToken', $page.url.pathname);
 
 		$UserStore = {
 			authenticated: false,
@@ -74,59 +80,102 @@
 
 	onMount(startSession);
 
-    // -----
+	// -----
 
-    async function readUsers() {
-        // Reads the User table using a filter.
-        const payload: UserFilter = {
-            id: 0,
-            is_enrolled: null,
-            is_active: null,
-            college: '',
-            unit: '',
-            program: '',
-            user_type: ''
-        }
+	async function readUsers() {
+		// Reads the User table using a filter.
+		const payload: UserFilter = {
+			id: 0,
+			is_enrolled: null,
+			is_active: null,
+			college: '',
+			unit: '',
+			program: '',
+			user_type: ''
+		};
 
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify(payload),
+		const response = await fetch('/api/user', {
+			method: 'POST',
+			body: JSON.stringify(payload),
 			headers: {
 				'content-type': 'application/json'
 			}
-        })
+		});
 
-        console.log((await response.json()))
-    }
+		console.log(await response.json());
+	}
 
-    async function loginRfid(rfid: string) {
-        // Logs in using user RFID and email from the database
-        const payload: {rfid: string} = {
-            rfid
-        }
+	async function loginRfid(rfid: string) {
+		// Logs in using user RFID and email from the database
+		// const { data, error } = await supabaseClient.auth.updateUser({ password: rfid})
+		// const { data, error } = await supabaseClient.auth.signUp({
+		// 	email: `${username}@up.edu.ph`,
+		// 	password: rfid
+		// });
+		// console.log(data)
+		// console.log(error)
+		const payload: { rfid: string } = {
+			rfid
+		};
 
-        const response = await fetch('/api/login', {
-            method: 'POST',
-            body: JSON.stringify(payload),
+		const response = await fetch('/api/login', {
+			method: 'POST',
+			body: JSON.stringify(payload),
 			headers: {
 				'content-type': 'application/json'
 			}
-        })
+		});
 
-        const { username } = await response.json()
+		const { username } = await response.json();
 
-        const { error } = await supabaseClient.auth.signInWithPassword({
-            email: `${username}@up.edu.ph`,
-            password: rfid
-        })
+		const { error } = await supabaseClient.auth.signInWithPassword({
+			email: `${username}@up.edu.ph`,
+			password: rfid
+		});
 
-        if (error) {
-            toast.error(`Error with logging in with RFID: ${error}`);
-        }
+		if (error) {
+			toast.error(`Error with logging in with RFID: ${error}`);
+		}
 
-        startSession();
-        return;
-    }
+		startSession();
+		return;
+	}
+
+	async function sendOtp(username: string) {
+		// Logs in using user email with OTP
+		console.log(`${username}@up.edu.ph`);
+		const { data, error } = await supabaseClient.auth.signInWithOtp({
+			email: `${username}@up.edu.ph`
+			// options: {
+			// 	// set this to false if you do not want the user to be automatically signed up
+			// 	shouldCreateUser: false
+			// }
+		});
+		console.log(data);
+		console.log(error);
+	}
+
+	async function loginOtp(otp: string) {
+		// Logs in using user email with OTP
+		const { data, error } = await supabaseClient.auth.verifyOtp({
+			email: `${username}@up.edu.ph`,
+			token: otp,
+			type: 'email'
+			// options: {
+			// 	// set this to false if you do not want the user to be automatically signed up
+			// 	shouldCreateUser: false
+			// }
+		});
+        console.log(data)
+
+		if (error) {
+			toast.error(`Error with verifying OTP: ${error}`);
+            return
+		}
+
+		startSession();
+		return;
+	}
 </script>
 
 <Toaster />
@@ -137,25 +186,38 @@
 
 	<!-- Select library and section to proceed to enable the website -->
 	<section class="flex grow flex-col items-center">
-		{#if $UserStore.fullName}
+		{#if $UserStore.authenticated}
 			<h1>You are now logged in, {$UserStore.fullName}</h1>
-            <button on:click={readUsers}>Read User Table</button>
+			<button on:click={readUsers}>Read User Table</button>
 		{:else}
 			<h1>You are not logged in.</h1>
 		{/if}
 
-        <!-- Login with Email and Password -->
-        <!-- <form method="POST" action="?/rfidauth" class="flex flex-col"> -->
-            <label for="rfid">RFID:</label>
-            <input type="text" id="rfid" name="rfid" class="border border-4" bind:value={rfid}>
-            <button on:click={() => {loginRfid(rfid)}}>Login with RFID</button>
-        <!-- </form> -->
+		<!-- Login with Email and Password -->
+		<label for="rfid">RFID:</label>
+		<input type="text" id="rfid" name="rfid" class="border border-4" bind:value={rfid} />
+		<button
+			on:click={() => {
+				loginRfid(rfid);
+			}}>Login with RFID</button
+		>
 
-		<!-- Login with Google OAuth-->
-		<form method="POST" action="?/googleauth">
-			<button>Login with UP Mail</button>
-		</form>
-        
+		<!-- Send OTP -->
+		<input type="text" class="border border-4" bind:value={username} />
+		<button
+			on:click={() => {
+				sendOtp(username);
+			}}>Send OTP to UP Mail</button
+		>
+
+		<!-- Login with OTP -->
+		<input type="text" class="border border-4" bind:value={otp} />
+		<button
+			on:click={() => {
+				loginOtp(otp);
+			}}>Login with OTP</button
+		>
+
 		<!-- Logout -->
 		<button on:click={endSession}>Log out</button>
 	</section>
