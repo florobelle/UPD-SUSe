a01f2bd9ec23c13f210d255be939c419644b1806
florobelle <zbnfloro@gmail.com>
2025-01-18 14:39:04 +0800

    fix: service store

--- src/lib/stores/ServiceStore.ts
+++ src/lib/stores/ServiceStore.ts
@@ -3,12 +3,24 @@ import { writable, type Writable } from "svelte/store";
 
 type ServiceInformation = {
     serviceTypes: Array<ServiceTypeTable>,
-    services: Array<ServiceView>
+    serviceCards: {[key: string]: ServiceCard},
+    serviceOptions: {[key: string]: Array<ServiceOption>},
 }
 
-export const ServiceStore: Writable<ServiceInformation> = writable({
-    serviceTypes: [],
-    services: [],
-})
+type ServiceCard = {
+    service_type: string,
+    service_type_id: number,
+    available_number: number,
+    service_img_src: string,
+}
+
+type ServiceOption = {
+    type: string;
+    label: string;
+    options: ServiceView[];
+    variant: 'default' | 'ghost';
+}
+
+export const ServiceStore: Writable<ServiceInformation> = writable()
 
 export const ServiceTableStore: Writable<Array<ServiceView>> = writable();
