4cf8eedc882c59a5c521a58911a3a11b754c8233
florobelle <zbnfloro@gmail.com>
2025-02-10 10:07:46 +0800

    feat: tlc route checking
    Also included the section in the welcome greeting in student dashboard

--- src/routes/[library]/[section]/+layout.ts
+++ src/routes/[library]/[section]/+layout.ts
@@ -2,23 +2,31 @@ import { error } from '@sveltejs/kit';
 import type { LayoutLoad } from '../$types';
 
 export const load: LayoutLoad = ({ params }) => {
-	if (params.library === 'engglib1') {
-		return {
-			libraryName: 'Engineering Library I',
-			libraryBuilding: ' 2/F Melchor Hall, ',
-			libraryStreet: 'S. Osmeña Avenue, UP Diliman',
-			librarySrc: '../../../photos/MH.jpg',
-			librarySection: params.section
-		};
-	}
-	if (params.library === 'engglib2') {
-		return {
-			libraryName: 'Engineering Library II',
-			libraryBuilding: 'G/F UP Alumni Engineers Centennial Hall,',
-			libraryStreet: 'Velasquez St., UP Diliman',
-			librarySrc: '../../../photos/AECH.jpeg',
-			librarySection: params.section
-		};
-	}
+    let section:string = '';
+    if (params.section === 'circulation') {
+        section = 'Circulation';
+    } else if (params.section === 'the-learning-commons') {
+        section = 'The Learning Commons';
+    }
+
+	if (section) {
+        if (params.library === 'engglib1') {
+            return {
+                libraryName: 'Engineering Library I',
+                libraryBuilding: ' 2/F Melchor Hall, ',
+                libraryStreet: 'S. Osmeña Avenue, UP Diliman',
+                librarySrc: '../../../photos/MH.jpg',
+                librarySection: section
+            };
+        } else if (params.library === 'engglib2') {
+            return {
+                libraryName: 'Engineering Library II',
+                libraryBuilding: 'G/F UP Alumni Engineers Centennial Hall,',
+                libraryStreet: 'Velasquez St., UP Diliman',
+                librarySrc: '../../../photos/AECH.jpeg',
+                librarySection: section
+            };
+        }
+    }
 	error(404, 'Not found');
 };
