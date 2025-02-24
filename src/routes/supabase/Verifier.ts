3ef0340d76929c7315a80b8ebbceca1ec97ce388
florobelle <zbnfloro@gmail.com>
2025-02-24 17:09:03 +0800

    fix: verify pc

--- src/routes/supabase/Verifier.ts
+++ src/routes/supabase/Verifier.ts
@@ -1,19 +1,21 @@
 import { createCookie, deleteCookie, readCookie } from "$lib/client/Cookie";
 import { supabaseClient } from "$lib/client/SupabaseClient";
 
-async function addPCCookie(): Promise<{ error:string }> {
+async function addPCCookie(): Promise<{ newID: string, error: string }> {
     // Adds a unique ID for a PC
-    const newID:string = Math.ceil(Math.random() * (10 ** 15)).toString();
+    const newID: string = Math.ceil(Math.random() * (10 ** 15)).toString();
     createCookie('pcCookie', newID, 24 * 30, '/')
-    
+
     const { error } = await supabaseClient.from('cookie').insert({ cookie_id: newID })
 
     if (error) {
         return {
+            newID: '',
             error: error.message
         }
     } else {
         return {
+            newID,
             error: ''
         }
     }
@@ -21,25 +23,23 @@ async function addPCCookie(): Promise<{ error:string }> {
 
 export async function verifyPC(): Promise<{ error: string }> {
     // Verifies this PC is an EnggLib managed PC by checking the generated cookie
-    let pcCookie:number = parseInt(readCookie('pcCookie'));
-    if (pcCookie) {
-        // console.log('readCookie', pcCookie)
-        const { data, error } = await supabaseClient
-            .from('cookie')
-            .select('is_allowed')
-            .eq('cookie_id', pcCookie)
-        if (error) {
-            return {
-                error: error.message
-            }
-        } 
-        else if (data.length && !data[0].is_allowed) {
-            return {
-                error: 'PC not allowed to access SUSê.'
-            }
+    let pcCookie: number = parseInt(readCookie('pcCookie'));
+    if (!pcCookie) {
+        pcCookie = parseInt((await addPCCookie()).newID);
+    }
+    const { data, error } = await supabaseClient
+        .from('cookie')
+        .select('is_allowed')
+        .eq('cookie_id', pcCookie)
+    if (error) {
+        return {
+            error: error.message
         }
-    } else {
-        addPCCookie();
+    }
+    else if (data.length && !data[0].is_allowed) {
+        return {
+            error: 'PC not allowed to access SUSê.'
+        }
     }
     return {
         error: ''
